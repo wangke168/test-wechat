9a2cc4f8899342014f4f9a3d42a9e8e513a71cde
wangke168 <wangke168@gmail.com>
2018-06-29 05:18:45 +0000

    test

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -7,12 +7,17 @@ use EasyWeChat\Factory;
 class ApiController extends Controller
 {
     public $weObj;
+    public $js;
     public function __construct()
     {
         $this->weObj=app('wechat.official_account');
+        $this->js=$this->weObj->js;
     }
     public function token()
     {
-        return $this->weObj->access_token->getToken();
+        $token=$this->weObj->access_token->getToken();
+        $ticket=$this->js->ticket();
+        $data=['token'=>$token,'ticket'=>$ticket];
+        return response()->json($data);
     }
 }
