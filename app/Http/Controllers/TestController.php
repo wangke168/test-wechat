ff7998e130f23d51c1abcfe386b66c2b866624ff
wangke168 <wangke168@gmail.com>
2018-07-25 15:46:06 +0800

    test

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -79,21 +79,37 @@ class TestController extends Controller
     public function index()
     {
         $message = new Text('FromUserName');
-        $news1 =
-            new NewsItem([
-                'title' => 'title',
-                'description' => '...',
-                'url' => 'url',
-                'image' => 'image',
-            ]);
-        $news2 =
-            new NewsItem([
-                'title' => 'title',
-                'description' => '...',
-                'url' => 'url',
-                'image' => 'image',
-            ]);
-        $news = new News([$news1, $news2]);
+        $row = WechatArticle::where('classid', '7')
+            ->usagePublished('all')
+            ->skip(0)->take(8)->get();
+
+        if ($row) {
+            $content = array();
+            foreach ($row as $result) {
+                $url = $result->url;
+                $id = $result->id;
+                /*如果只直接跳转链接页面时，判断是否已经带参数*/
+                if ($url != '') {
+                    /*链接跳转的数据统计*/
+                    $url = "https://" . $_SERVER['HTTP_HOST'] . "/jump/{$id}";
+
+                } else {
+                    $url = "https://" . $_SERVER['HTTP_HOST'] . "/article/detail?id=" . $id . "&wxnumber=";
+                }
+
+                $pic_url = "https://wx-control.hdyuanmingxinyuan.com/" . $result->picurl;
+
+                $items = new NewsItem([
+                    'title' => $result->title,
+                    'description' => $result->description,
+                    'url' => $url,
+                    'image' => $pic_url,
+                ]);
+
+                $content[] = $items;
+            }
+            $news = new News($content);
+        }
         $result = $this->weObj->customer_service->message($news)->to('o5--l1Pl9YZWPj9n342XbdpJdG8w')->send();
 
     }
