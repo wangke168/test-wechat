550081d7bfbfbf7d69a0580501589a5dd4bee56e
wangke168 <wangke168@gmail.com>
2018-07-25 14:52:45 +0800

    test

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use EasyWeChat\Factory;
 use EasyWeChat\Kernel\Messages\Text;
+use App\Models\WechatArticle;
+use EasyWeChat\Kernel\Messages\News;
+use EasyWeChat\Kernel\Messages\NewsItem;
 class TestController extends Controller
 {
     public $weObj;
@@ -13,6 +16,51 @@ class TestController extends Controller
     {
         $this->weObj=app('wechat.official_account');
     }
+    public function response_test()
+    {
+        $row = WechatArticle::where('classid', '7')
+            ->usagePublished('all')
+            ->skip(0)->take(8)->get();
+
+        if ($row) {
+            $content = array();
+            foreach ($row as $result) {
+                $url = $result->url;
+                $id = $result->id;
+                /*如果只直接跳转链接页面时，判断是否已经带参数*/
+                if ($url != '') {
+                    /*链接跳转的数据统计*/
+                    $url = "https://" . $_SERVER['HTTP_HOST'] . "/jump/{$id}";
+
+                } else {
+                    $url = "https://" . $_SERVER['HTTP_HOST'] . "/article/detail?id=" . $id . "&wxnumber=" ;
+                }
+
+                $pic_url = "https://wx-control.hdyuanmingxinyuan.com/" . $result->picurl;
+
+                /*索引图检查结束*/
+                /*                $new = new News();
+                                $new->title = $result->title;
+                                $new->description = $result->description;
+                                $new->url = $url;
+                                $new->image = $pic_url;
+                                $content[] = $new;*/
+
+                $items = new NewsItem([
+                    'title' => $result->title,
+                    'description' => $result->description,
+                    'url' => $url,
+                    'image' => $pic_url,
+                    // ...
+                ]);
+                $content[] = $items;
+            }
+            $news = new News($content);
+        }
+        return($news);
+    }
+
+
     public function index()
     {
         $message = new Text('FromUserName');
