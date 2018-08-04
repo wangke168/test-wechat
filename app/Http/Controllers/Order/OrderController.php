9c3be7a95cc7e1c014919e6f506117e7f70c06db
wangke168 <wangke168@gmail.com>
2018-08-04 16:38:39 +0800

    test

--- app/Http/Controllers/Order/OrderController.php
+++ app/Http/Controllers/Order/OrderController.php
@@ -204,7 +204,7 @@ class OrderController extends Controller
                     "keyword2" => [$date, "#173177"],
                     "keyword3" => [$ticket, "#173177"],
                     "keyword4" => [$numbers, "#173177"],
-                    "keyword5" => [$ticketorder, "#173177"],
+                    "keyword5" => [$ticketorder, "#F00"],
                     "remark" => [$remark, "#000000"],
                 ];
             }
