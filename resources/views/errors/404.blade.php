4b11704a2e9c5b20621c455c21dd3f98c6d4d189
wangke168 <wangke168@gmail.com>
2018-08-07 10:38:51 +0800

    test

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,28 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head id="Head2">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="format-detection" content="telephone=no" />
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0" />
+    <meta name="MobileOptimized" content="320"/>
+    <meta name="copyright" content="Copyright &copy; 2013 hengdianworld.com Inc. All Rights Reserved." />
+    <title>出错了</title>
+    <link href="{{asset('css/mbcss.css')}}" rel="stylesheet" type="text/css" />
+    <style type="text/css" >
+        img{max-width: 100%;}
+    </style>
+</head>
+<body>
+<div id="main">
+    <div id="header" >
+    </div>
+    <div id="title"></div>
+    <div id="titleinfo">
+    </div>
+    <div id="contents">
+        <img src="{{asset('images/error.jpg')}}" width="100%">
+    </div>
+
+</body>
+</html>
+
+
