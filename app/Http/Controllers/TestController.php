82dec14747dac1e150480e985776f64c075602c5
wangke168 <wangke168@gmail.com>
2019-06-11 18:09:47 +0800

    test

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -9,17 +9,61 @@ use EasyWeChat\Kernel\Messages\Text;
 use App\Models\WechatArticle;
 use EasyWeChat\Kernel\Messages\News;
 use EasyWeChat\Kernel\Messages\NewsItem;
+use Intervention\Image\Facades\Image;
 use App\WeChat\Zone;
 class TestController extends Controller
 {
     public $weObj;
     public $config;
+    public $qrcode;
     public $templateId;
     public function __construct()
     {
         $this->weObj = app('wechat.official_account');
         $this->templateId=env('TEST_TEMPLATEID_TICKET');
     }
+
+
+    public function create()
+    {
+        $result = $this->weObj->qrcode->forever(100);// 或者 $app->qrcode->forever("foo");
+        $ticket = $result->ticket;
+        $url = $this->weObj->qrcode->url($ticket);
+        return $url;
+        $content = file_get_contents($url);
+
+        $id='101';
+        $row=DB::table('wx_qrscene_info')
+            ->where('qrscene_id',$id)
+            ->first();
+        $app = app('wechat');
+        $qrcode = $this->weObj->qrcode;
+        $result = $qrcode->forever($id);// 或者 $qrcode->forever("foo");
+        $ticket = $result->ticket; // 或者 $result['ticket']
+        if ($row->qrscene_logo) {
+            $qr_logo = $row->qrscene_logo;
+        }
+        else{
+            $qr_logo='qr/logo.png';
+        }
+        /* $QR = $qrcode->url($ticket);
+         $logo = $qr_logo;
+         $img = Image::make($QR);
+         $img->insert($logo, 'center');
+         return $img->response('png');*/
+        return $this->create_qr($ticket,$qr_logo);
+    }
+    private function create_qr($ticket,$qr_logo)
+    {
+        $QR = $this->qrcode->url($ticket);
+        $logo=$qr_logo;
+        $img = Image::make($QR);
+        $img->insert($logo, 'center');
+        return $img->response('png');
+    }
+
+
+
 
     public function tag(Request $request)
     {
