228b6c05b8d566a6335980e67cf94909cb136f63
wangke168 <wangke168@gmail.com>
2019-06-11 14:13:47 +0800

    test

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -58,62 +58,87 @@ class MenuController extends Controller
     {
         $buttons = [
             [
-                "name" => "圆明新园",
+                "name" => "会议会展",
                 "sub_button" => [
                     [
                         "type" => "view",
-                        "name" => "景区简介",
-                        "url" => "https://wechat.hdyuanmingxinyuan.com/info_jq_detail"
+                        "name" => "酒店会场",
+                        "url" => "https://mp.weixin.qq.com/s/uYGh32ht_Tz0XkIRbwJoug"
                     ],
                     [
                         "type" => "view",
-                        "name" => "地图导览",
-                        "url" => "http://nwx.weijingtong.net/map/206"
+                        "name" => "景区剧场",
+                        "url" => "https://mp.weixin.qq.com/s/blcC9BMEDviELXFoonYxdg"
                     ],
                     [
                         "type" => "view",
-                        "name" => "游玩攻略",
-                        "url" => "http://nwx.weijingtong.net/corpus/360"
+                        "name" => "特色资源",
+                        "url" => "https://mp.weixin.qq.com/s/vyKJt8EtOa0lAbvHDFgjug"
+                    ],
+                    [
+                        "type" => "view",
+                        "name" => "穿越主题会议",
+                        "url" => "https://mp.weixin.qq.com/s/GljFzb8Ygib_Dq0DdEI7Tw"
+                    ],
+                    [
+                        "type" => "view",
+                        "name" => "成功案例",
+                        "url" => "https://mp.weixin.qq.com/s/d48y9Gso3MuaZqcUZsC6Rw"
                     ],
                 ],
             ],
             [
-                "name" => "我要预订",
+                "name" => "活动赛事",
                 "sub_button" => [
                     [
                         "type" => "click",
-                        "name" => "门票预订",
+                        "name" => "品牌赛事",
                         "key" => "7"
                     ],
                     [
-                        "type" => "click",
-                        "name" => "年卡办理",
-                        "key" => "23"
+                        "type" => "view",
+                        "name" => "儿童电影节",
+                        "key" => "https://mp.weixin.qq.com/s/8GWBO7eNtkk9AMl4TpM4sQ"
                     ],
                     [
                         "type" => "view",
-                        "name" => "订单查询",
-                        "url" => "http://ydpt.hdyuanmingxinyuan.com/yd_search.aspx"
+                        "name" => "影视旅游小姐大赛",
+                        "url" => "https://mp.weixin.qq.com/s/AQKGow97mWlyZC2h41y4Og"
+                    ],
+                    [
+                        "type" => "view",
+                        "name" => "横店影视武林会",
+                        "url" => "https://mp.weixin.qq.com/s/PNxklfedA_iSHwIqFrkzyA"
+                    ],
+                    [
+                        "type" => "click",
+                        "name" => "童星盛典",
+                        "url" => "8"
                     ],
                 ],
             ],
             [
-                "name" => "游园指南",
+                "name" => "疗休养",
                 "sub_button" => [
                     [
-                        "type" => "click",
-                        "name" => "客服电话",
-                        "key" => "13"
-                    ],
-                    [
-                        "type" => "click",
-                        "name" => "节目时间表",
-                        "key" => "14"
+                        "type" => "view",
+                        "name" => "横店景区简介",
+                        "key" => "http://m.hengdianworld.com/info_jq.aspx"
                     ],
                     [
                         "type" => "view",
-                        "name" => "交通速查",
-                        "url" => "https://wechat.hdyuanmingxinyuan.com/article/detail?id=1492"
+                        "name" => "演艺秀简介",
+                        "key" => "http://m.hengdianworld.com/info_yyx.aspx"
+                    ],
+                    [
+                        "type" => "view",
+                        "name" => "尊享卡",
+                        "url" => "https://mp.weixin.qq.com/s/kpUcrdVdAfOdsnZE_bJBSA"
+                    ],
+                    [
+                        "type" => "view",
+                        "name" => "横店疗休养",
+                        "url" => "https://mp.weixin.qq.com/s/UOVSMJNaVxTBrDt984vHPw"
                     ],
                     /*      [
                               "type" => "click",
